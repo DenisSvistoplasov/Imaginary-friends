649e1beb3817c72e5a75d4b40464dbbed068b968
DenisSvistoplasov <57819405+DenisSvistoplasov@users.noreply.github.com>
2024-10-29 19:45:54 +0000

    test commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 export function Header({}: HeaderProps) {
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.title}>Imaginary friends</h1>
+      <h1 className={styles.title}>Imaginary friends!</h1>
     </div>
   );
 }
